e909a083596e74d831db65cb9833b58d930c5338
iamrkay786 <radhamusicbot5911@gmail.com>
2025-03-04 10:24:09 +0530

    Update and rename welcome.py# to welcome.py

--- SONALI_MUSIC/Plugins/tools/welcome.py
+++ SONALI_MUSIC/Plugins/tools/welcome.py
@@ -16,7 +16,7 @@ from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from logging import getLogger
-from SONALI_MUSIC.utils.RAUSHAN_ban import admin_filter
+from SONALI_MUSIC.utils.RAUSHAN_BAN import admin_filter
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
