07fe72a1bbe0f7467e37cf3b270d190a06a8a1e0
iamrkay786 <radhamusicbot5911@gmail.com>
2025-03-17 20:05:39 +0530

    Update info.py

--- SONALI_MUSIC/Plugins/mics/info.py
+++ SONALI_MUSIC/Plugins/mics/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Sonali_Music_Bot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/radha_music_bot?startgroup=true"),
     ],
 ]
 
